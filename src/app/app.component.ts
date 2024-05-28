c7eec25b3839b4bb8283d021a3fab47dc30f369b
fumimelvindev <fumimelvindev@gmail.com>
2024-05-28 17:50:22 +0200

    test vercel with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, HeroComponent, AboutComponent, ProjectsComponent],
 })
 export class AppComponent {
   title = 'portfolio';
